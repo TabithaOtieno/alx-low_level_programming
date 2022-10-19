c845ebe1bf5962deb894e1480d531f4ac55df981
TabithaOtieno <otienotabby26@gmail.com>
2022-10-19 14:18:21 -0700

    Task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+*print_times_table - print the n times table
+*@n: integer times table eing printed
+*/
+void print_times_table(int n)
+{
+	if (n > 15 || n <  0)
+	{
+		_putchar(' ');
+	}
+	else
+	{
+		int x, y, z;
+
+		for (x = 0; x <= n; x++)
+		{
+			_putchar('0');
+			for (y = 1; y <= n; y++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				z = x * y;
+				if (z <= 9)
+					_putchar(' ');
+				else
+					_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+			}
+		_putchar('\n');
+		}
+	}
+}
